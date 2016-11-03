6c67838cae587e4d0fda49eb35bdef333a852bc9
Jim Vaughan <jvaughan3@gmail.com>
2016-11-02 22:42:36 -0500

    move to folder matching the name of the algorithm

---
+++ 02_selection_sort/csharp/01_selection_sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new List<int> { 5, 3, 6, 2, 10 };
+            Console.WriteLine(string.Join(", ", selection_sort(arr)));
+        }
+
+        private static int[] selection_sort(List<int> arr)
+        {
+            int[] newArr = new int[arr.Count];
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                int smallest = findSmallest(arr);
+                newArr[i] = arr[findSmallest(arr)];
+                arr.RemoveAt(smallest);
+            }
+            return newArr;
+        }
+
+        private static int findSmallest(List<int> arr)
+        {
+            var smallest = arr[0];
+            var smallest_index = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] < smallest)
+                {
+                    smallest = arr[i];
+                    smallest_index = i;
+                }
+            }
+            return smallest_index;
+        }
+    }
+}
