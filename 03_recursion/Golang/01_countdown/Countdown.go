630641de9f61a4cc83c14398c203409efff1feac
seong954t <seomtae@naver.com>
2017-10-14 13:29:45 +0900

    go_fmt

--- 03_recursion/Golang/01_countdown/Countdown.go
+++ 03_recursion/Golang/01_countdown/Countdown.go
@@ -2,16 +2,15 @@ package main
 
 import "fmt"
 
-func countdown(i int){
-    fmt.Println(i)
-    if i <= 0 {
-	return
-    }else{
-        countdown(i-1)
-    }
+func countdown(i int) {
+	fmt.Println(i)
+	if i <= 0 {
+		return
+	} else {
+		countdown(i - 1)
+	}
 }
 
 func main() {
-    countdown(5);
+	countdown(5)
 }
-
--- 03_recursion/Golang/03_factorial/Factorial.go
+++ 03_recursion/Golang/03_factorial/Factorial.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func fact(x int) int {
-    if x == 1 {
-	return 1
-    }else{
-        return x * fact(x-1)
-    }
+	if x == 1 {
+		return 1
+	} else {
+		return x * fact(x-1)
+	}
 }
 
 func main() {
-    fmt.Println(fact(5))
+	fmt.Println(fact(5))
 }
-
