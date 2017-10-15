267f4eb6661177cb274c254ead7178d067e47581
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-15 14:52:46 +0300

    fix algoritm

--- 04_quicksort/es6/quicksort.js
+++ 04_quicksort/es6/quicksort.js
@@ -1,18 +1,11 @@
 const quickSort = (array) => {
-	if (array.length < 2) {
-		return array;
-	}
-	if (array.length === 2) {
-		// if first elem more than second will swap them and return it like new array.
-		return array[0] < array[1] ? array : [array[1], array[0]];
-	}
-
-	const pivot = array[0]
-	const itemsAreLessPivotSubArray = array.filter(item => item < pivot);
-	const itemsAreMoreThenPivotSubArray = array.filter(item => item > pivot);
-
-	return [...quickSort(itemsAreLessPivotSubArray), pivot, ...quickSort(itemsAreMoreThenPivotSubArray)];
+  if (array.length < 2) {
+    return array;
+  }
+  const pivot = array[0];
+  const itemsAreLessPivot = array.slice(1).filter(item => item <= pivot);
+  const itemsAreMorePivot = array.slice(1).filter(item => item > pivot);
+  return [...quickSort(itemsAreLessPivot), pivot, ...quickSort(itemsAreMorePivot)];
 };
 
-module.exports = quickSort;
-console.log(quicksort([10, 5, 2, 3])); // [2, 3, 5, 10]
+quicksort([10, 5, 2, 3])); // [2, 3, 5, 10]
