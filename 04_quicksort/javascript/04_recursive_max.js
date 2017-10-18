237534c575f73b3160ecdde18c366985f63f75b0
庄嘉祥 <411546535@qq.com>
2017-10-18 06:33:47 -0500

    Update 04_recursive_max.js

--- 04_quicksort/javascript/04_recursive_max.js
+++ 04_quicksort/javascript/04_recursive_max.js
@@ -9,3 +9,20 @@ function max(list) {
 }
 
 console.log(max([1, 5, 10, 25, 16, 1])); // 25
+
+
+
+
+
+//max([1]);VM819:1 Uncaught RangeError: Maximum call stack size exceeded
+/** my code
+function max(arr){
+	if(arr.length===1){
+		return arr[0];
+	}
+	let maxNumber = max(arr.slice(1));
+	return arr[0]>maxNumber ? arr[0] : maxNumber;
+}
+
+
+**/
