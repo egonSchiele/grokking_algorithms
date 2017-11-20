732cb52e760377540f66be08856643f2574d7d08
seong954t <seomtae@naver.com>
2017-11-20 15:18:37 +0900

    golang 05_hash_tables 01_price_of_groceries

--- 05_hash_tables/golang/02_check_voter.go
+++ 05_hash_tables/golang/02_check_voter.go
@@ -5,17 +5,17 @@ import "fmt"
 var voted map[string]bool
 
 func main() {
-    voted = make(map[string]bool)
-    check_voter("tom")
-    check_voter("mike")
-    check_voter("mike")
+	voted = make(map[string]bool)
+	check_voter("tom")
+	check_voter("mike")
+	check_voter("mike")
 }
 
-func check_voter(name string){
-    if voted[name]{
-    	fmt.Println("kick tem out!")
-    }else{
-    	voted[name] = true
-	    fmt.Println("let tem vote!")
+func check_voter(name string) {
+	if voted[name] {
+		fmt.Println("kick tem out!")
+	} else {
+		voted[name] = true
+		fmt.Println("let tem vote!")
 	}
 }
