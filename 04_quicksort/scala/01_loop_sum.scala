6794fec8e20daec45daff8170ad07eb6500105f5
chase-g <chasegorland@gmail.com>
2017-12-06 23:48:58 -0500

    Update 01_loop_sum.scala

--- 04_quicksort/scala/01_loop_sum.scala
+++ 04_quicksort/scala/01_loop_sum.scala
@@ -1,10 +1,9 @@
 object Sum {
   def main(args: Array[String]): Unit = {
-    def sum(arr: List[Int], total: Int = 0): Int = {
-      // if the list is not empty, call recursively on the list's tail with the sum of the accumulated value and list's head
-      if (!arr.isEmpty) sum(arr.tail, total + arr.head)
-      // otherwise, return the accumulated value
-      else total
+    def sum(arr: List[Int]): Int = {
+      var total: Int = 0
+      for (x <- arr) total += x
+      total
     }
     println(sum(List(1, 2, 3, 4)))
   }
