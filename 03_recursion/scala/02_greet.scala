4ac4d4a263416384875ebc71b34b378220cb67a6
chase-g <chasegorland@gmail.com>
2017-12-06 23:23:03 -0500

    Update 02_greet.scala

--- 03_recursion/scala/02_greet.scala
+++ 03_recursion/scala/02_greet.scala
@@ -1,21 +1,17 @@
 object Greet {
   def main(args: Array[String]): Unit = {
-
     def greet2(name: String): Unit = {
       println("how are you, " + name + "?")
     }
-
     def bye(): Unit = {
       println("ok bye!")
     }
-
     def greet(name: String): Unit = {
       println("hello, " + name + "!")
       greet2(name)
       println("getting ready to say bye...")
       bye()
     }
-
     greet("adit")
   }
 }
