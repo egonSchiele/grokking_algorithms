7541a09b48b24c26ef1f430a4df8ca48ff9c5df8
chase-g <chasegorland@gmail.com>
2017-12-06 23:54:44 -0500

    Update 03_recursive_count.scala

--- 04_quicksort/scala/03_recursive_count.scala
+++ 04_quicksort/scala/03_recursive_count.scala
@@ -1,4 +1,9 @@
-def count(list: List[Int]): Int = {
-  if(list.isEmpty) return 0 
-  return 1 + count(list.drop(1))
+object Count {
+  def main(args: Array[String]): Unit = {
+    def count(list: List[Int]): Int = {
+      if (list.isEmpty) return 0
+      return 1 + count(list.drop(1))
+    }
+    println(count(List(1, 4, 7, 8)))
+  }
 }
