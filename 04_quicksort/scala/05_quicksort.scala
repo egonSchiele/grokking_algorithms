857d879049df578388eb696edaa184911eefa57b
chase-g <chasegorland@gmail.com>
2017-12-08 19:24:45 -0500

    Update 05_quicksort.scala

--- 04_quicksort/scala/05_quicksort.scala
+++ 04_quicksort/scala/05_quicksort.scala
@@ -1,16 +1,16 @@
-def quicksort(array: Array[Int]): Array[Int] = {
-  if(array.length < 2){
-    //base case, arrays with 0 or 1 element are already "sorted"
-    return array
-  }
-  else {
-    //recursive case
-    val pivot: Int = array(0)
-    //sub-array of all the elements less than the pivot
-    val less: Array[Int] = array.drop(1).filter(x => x < pivot)
-    //sub-array of all the elements greater than the pivot
-    val greater: Array[Int] = array.drop(1).filter(x => x > pivot)
-    return (quicksort(less) :+ pivot) ++ quicksort(greater)
+object quicksort {
+  def main(args: Array[String]): Unit = {
+    def quicksort(array: Array[Int]): Array[Int] = {
+      if (array.length < 2) { //base case, arrays with 0 or 1 element are already "sorted"
+        return array
+      } else {
+        val pivot: Int = array(0) //recursive case
+        val less: Array[Int] = array.drop(1).filter(x => x < pivot) //sub-array of all the elements less than the pivot
+        val greater: Array[Int] = array.drop(1).filter(x => x > pivot) //sub-array of all the elements greater than the pivot
+        return (quicksort(less) :+ pivot) ++ quicksort(greater)
+      }
+    }
+    val answer = quicksort(Array(10, 5, 2, 3))
+    for (i <- answer) println(i)
   }
 }
-quicksort(Array(10,5,2,3))
