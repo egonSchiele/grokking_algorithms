59b29c369f1c9c50b08d9dccd3fc2fb6b31c471d
chase-g <chasegorland@gmail.com>
2017-12-09 13:33:58 -0500

    Update 01_selection_sort.scala

--- 02_selection_sort/scala/01_selection_sort.scala
+++ 02_selection_sort/scala/01_selection_sort.scala
@@ -1,25 +1,30 @@
-//Finds the smallest value in an array
-def findSmallest(arr: Array[Int]): Int = {
-  //Stores the smallest value
-  var smallest = arr(0)
-  for(i <- 1 until arr.length){
-    if(arr(i) < smallest) {
-      smallest = arr(i)
+object SelectionSort {
+  def main(args: Array[String]): Unit = {
+    //Finds the smallest value in an array
+    def findSmallest(arr: Array[Int]): Int = {
+      //Stores the smallest value
+      var smallest = arr(0)
+      for (i <- 1 until arr.length) {
+        if (arr(i) < smallest) {
+          smallest = arr(i)
+        }
+      }
+      return smallest
     }
-  }
-  return smallest
-}
 
-//Sort array
-def selectionSort(arr: Array[Int]): Array[Int] = {
-  val newArr = new Array[Int](arr.length)
-  for(i <- 0 until arr.length){
-    //Finds the smallest element in the array and adds it to the new array
-    val smallest = findSmallest(arr)
-    newArr(i) = smallest
-    //Sets current smallest value in array above largest value to avoid reusing values
-    arr(arr.indexOf(smallest)) = arr.max + 1
+    //Sort array
+    def selectionSort(arr: Array[Int]): Array[Int] = {
+      val newArr = new Array[Int](arr.length)
+      for (i <- 0 until arr.length) {
+        //Finds the smallest element in the array and adds it to the new array
+        val smallest = findSmallest(arr)
+        newArr(i) = smallest
+        //Sets current smallest value in array above largest value to avoid reusing values
+        arr(arr.indexOf(smallest)) = arr.max + 1
+      }
+      return newArr
+    }
+    val sorted = selectionSort(Array(5, 3, 6, 2, 10))
+    for(i <- sorted) println(i)
   }
-  return newArr
 }
-selectionSort(Array(5, 3, 6, 2, 10))
