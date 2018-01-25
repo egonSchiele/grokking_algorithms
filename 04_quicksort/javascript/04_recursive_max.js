e749abea024cf1f89a0197a6fb01f70c286e2246
PlayerWho <411546535@qq.com>
2018-01-25 17:16:12 +0800

    Update 04_recursive_max.js
    fix bug

--- 04_quicksort/javascript/04_recursive_max.js
+++ 04_quicksort/javascript/04_recursive_max.js
@@ -1,28 +1,11 @@
 'use strict';
 
 function max(list) {
-  if (list.length === 2) {
-    return list[0] > list[1] ? list[0] : list[1];
-  }
+   if(list.length===1){
+	return list[0];
+   }
   let sub_max = max(list.slice(1));
   return list[0] > sub_max ? list[0] : sub_max;
 }
 
 console.log(max([1, 5, 10, 25, 16, 1])); // 25
-
-
-
-
-
-//max([1]);VM819:1 Uncaught RangeError: Maximum call stack size exceeded
-/** my code
-function max(arr){
-	if(arr.length===1){
-		return arr[0];
-	}
-	let maxNumber = max(arr.slice(1));
-	return arr[0]>maxNumber ? arr[0] : maxNumber;
-}
-
-
-**/
