6cbfb41301a9307057e4a65b6a338306e16114a1
Artem Solovev <artem.solovev.lse@gmail.com>
2018-04-20 10:15:49 +0300

    Added another loop implementation sum func by reduce (JS)

--- 04_quicksort/javascript/01_loop_sum.js
+++ 04_quicksort/javascript/01_loop_sum.js
@@ -1,11 +1,34 @@
-'use strict';
+const arr = [1, 2, 3, 4];
 
-function sum(arr) {
-  let total = 0;
-  for (let x = 0; x < arr.length; x++) {
-    total += arr[x];
-  }
-  return total;
-}
+/**
+ * Sums values in array by loop "for"
+ * @param {Array} arr Array of numbers
+ * @return {number} Sum of the numbers
+ */
+const sumLoop = ( arr ) => {
+    const newArr = Array.prototype.slice.call( arr );
+    let result = 0;
 
-console.log(sum([1, 2, 3, 4])) // 10
+    for ( let i = 0; i < newArr.length; i++ ) {
+        result += newArr[i];
+    }
+
+    return result;
+};
+
+/**
+ * Sums values in array by function "reduce"
+ * @param {Array} arr Array of numbers
+ * @return {number} Sum of the numbers
+ */
+const sumReduce = ( arr ) => {
+    const newArr = Array.prototype.slice.call( arr );
+    let result = newArr.reduce( ( curr, prev ) => {
+        return curr + prev;
+    } );
+
+    return result;
+};
+
+console.log( sumLoop( arr ) ); // 10
+console.log( sumReduce( arr ) ); // 10
