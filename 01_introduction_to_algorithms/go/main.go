78708956edd543583d05f8839b06b3140cfc8d2c
Markustak <policy308@gmail.com>
2019-06-15 01:40:47 +0200

    added Go solution for chapter 01

---
+++ 01_introduction_to_algorithms/go/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Creating data to search trough
+	sliceToSearch := []int{1, 3, 5, 7, 9}
+	//What we are searching for
+	target := 9
+
+	position := binarySearch(sliceToSearch, target)
+
+	if position < 0 {
+		fmt.Printf("%v doesn't exist in the slice %v\n", target, sliceToSearch)
+	} else {
+		fmt.Printf("Found %v in position: %v \n", target, position)
+	}
+}
+
+func binarySearch(list []int, target int) int {
+	low := 0
+	high := len(list)
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		guess := list[mid]
+
+		if guess == target {
+			return mid
+		}
+
+		if guess > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
