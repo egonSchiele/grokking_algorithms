2aea98dd7bf32e64bb228624bbd42073df0be878
Matvey <38750524+mtovts@users.noreply.github.com>
2021-09-24 04:42:48 +0300

    Fix case when entrypoint is target

--- 06_breadth-first_search/python/01_breadth-first_search.py
+++ 06_breadth-first_search/python/01_breadth-first_search.py
@@ -1,7 +1,10 @@
 from collections import deque
 
 def person_is_seller(name):
-      return name[-1] == 'm'
+    if name[-1] == 'm':
+        print(name + " is a mango seller!")
+        return True
+    return False
 
 graph = {}
 graph["you"] = ["alice", "bob", "claire"]
@@ -15,16 +18,18 @@ graph["jonny"] = []
 
 def search(name):
     search_queue = deque()
+    if person_is_seller(name):
+        return True
+    searched = {name}
     search_queue += graph[name]
     # This is how you keep track of which people you've searched before.
-    searched = set()
+
     while search_queue:
         person = search_queue.popleft()
         # Only search this person if you haven't already searched them.
         if person in searched:
             continue
         if person_is_seller(person):
-            print(person + " is a mango seller!")
             return True
         search_queue += graph[person]
         # Marks this person as searched
