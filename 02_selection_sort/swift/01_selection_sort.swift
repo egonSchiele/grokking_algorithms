ae77c6de01fe854dd816dba63ce443414b1559e8
Artem <arti-0@mail.ru>
2022-05-03 18:39:15 +0400

    Chapter 2 for swift has been changed

--- 02_selection_sort/swift/01_selection_sort.swift
+++ 02_selection_sort/swift/01_selection_sort.swift
@@ -1,37 +1,47 @@
-import Foundation
+// Chapter 2
+// Selection sorting:
 
-// Finds the smallest value in an array
-func findSmallestIndex <T: Comparable> (_ arr: [T]) -> Int {
-    // Stores the smallest value
-    var smallest = arr[0]
-    // Stores the index of the smallest value
-    var smallestIndex = 0
-    for i in 1..<arr.count {
-        if arr[i] < smallest {
-            smallest = arr[i]
-            smallestIndex = i
-        }
+let arrayOne = [21, 10, 41, 4, 5, 0, 100, 1]
+
+// Find smallest element index:
+@discardableResult func smallestElementIndex<T: Comparable>(in array: [T]) -> Int? {
+    /// Checking array. It's empty or not.
+    guard !array.isEmpty, var smallestElement = array.first else { return nil }
+    
+    /// We might loop elements and then find smallest element.
+    array.forEach {
+        guard smallestElement >= $0 else { return }
+        smallestElement = $0
     }
-    return smallestIndex
+    
+    /// We might find smallest element also by that way.
+    /// P.S We use unwrap because we know exactly we have not empty array.
+//    smallestElement = array.sorted(by: <).first!
+    
+    /// By making use of native method as 'firstIndex' we can get needed index.
+    return array.firstIndex(of: smallestElement)
 }
 
-// Sort array
-func selectionSort <T: Comparable> (arr: [T]) -> [T] {
-    // We don't need any calculation if the array length is 1
-    guard arr.count > 1 else { return arr }
+smallestElementIndex(in: arrayOne) // => The result index will be '5'
+
+// Select sort from lowest to highest elements:
+@discardableResult func selectionSortByMin<T: Comparable>(in array: [T]) -> [T]? {
+    /// Checking array. It's empty or not.
+    guard !array.isEmpty else { return nil }
     
-    var newArr: [T] = []
+    /// Create 'mutableArray' for the mutable state of array from parameters.
+    var mutableArray = array
+    /// Create 'sortedArray' for the sorted elements
+    var sortedArray: [T] = []
     
-    // We have to make mutableArray reference copy of original array, because Swift 3 doesn't allow to get var parameter
-    var mutableArr = arr
-    for _ in 0..<mutableArr.count {
-       //Finds the smallest element in the array and adds it to the new array
-        let smallestIndex = findSmallestIndex(mutableArr)
-        newArr.append(mutableArr.remove(at: smallestIndex))
+    /// Make loop for the getting new 'sortedArray'
+    array.forEach { _ in
+        guard let smallestElementIndex = smallestElementIndex(in: mutableArray) else { return }
+        sortedArray.append(mutableArray[smallestElementIndex])
+        mutableArray.remove(at: smallestElementIndex)
     }
-    return newArr
+        
+    return sortedArray
 }
 
-print(selectionSort(arr: [5, 3, 6, 2, 10])) // => [2, 3, 5, 6, 10]
-
-
+selectionSortByMin(in: arrayOne) // => The result will be [0, 1, 4, 5, 10, 21, 41, 100]
