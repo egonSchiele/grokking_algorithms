1fc9aa50b8d131a2d243a4758c2a2b84fdd960e6
Artem <arti-0@mail.ru>
2022-05-11 19:45:47 +0400

    Some changes in recursive file

--- 04_quicksort/swift/02_recursive_sum.swift
+++ 04_quicksort/swift/02_recursive_sum.swift
@@ -10,3 +10,22 @@ func sum(list : [Int]) -> Int {
 }
 
 print(sum(list: [1, 2, 3, 4])) // => 10
+
+
+// MARK: - Another implementation:
+let arrayOne = [2.0, 4.7, 231, 1, 6, 8, 0, 4.1, 0.4, 100]
+
+// Method for finding the sum:
+@discardableResult func summarize<T: Numeric>(_ array: [T]) -> T {
+    /// Checking for empty array and unwraping of 'sum' property.
+    /// Base case.
+    guard !array.isEmpty else { return 0 }
+    /// In swift all method's parameters are constants, so we've created 'mutubleArray'.
+    var mutubaleArray = array
+    
+    /// Recursive case.
+    mutubaleArray.removeFirst()
+    return array.first! + summarize(mutubaleArray)
+}
+
+summarize(arrayOne) // Result => 357.2
--- 04_quicksort/swift/03_recursive_count.swift
+++ 04_quicksort/swift/03_recursive_count.swift
@@ -10,3 +10,22 @@ func count(list : [Int]) -> Int {
 }
 
 print(count(list: [1, 2, 3, 4])) // => 4
+
+
+// MARK: - Another implementation:
+let arrayOne = [2.0, 4.7, 231, 1, 6, 8, 0, 4.1, 0.4, 100]
+
+// Method for finding the array's count:
+@discardableResult func count<T>(of array: [T]) -> Int {
+    /// Checking for empty array.
+    /// Base case.
+    guard !array.isEmpty else { return 0 }
+    /// In swift all method's parameters are constants, so we've created 'mutubleArray'.
+    var mutubleArray = array
+    
+    /// Recursive case
+    mutubleArray.removeFirst()
+    return 1 + count(of: mutubleArray)
+}
+
+count(of: arrayOne) // Result => 10
