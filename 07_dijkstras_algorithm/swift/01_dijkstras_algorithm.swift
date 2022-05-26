b185af5164415e05ba194a070074b013a85cb46a
Artem <arti-0@mail.ru>
2022-05-26 13:48:20 +0400

    dijkstras algorithm was added

--- 07_dijkstras_algorithm/swift/01_dijkstras_algorithm.swift
+++ 07_dijkstras_algorithm/swift/01_dijkstras_algorithm.swift
@@ -77,3 +77,130 @@ while !node.isEmpty {
 print("Cost from the start to each node:")
 print(costs) // -> ["b": 2.0, "fin": 6.0, "a": 5.0]
 
+
+// MARK: - Another implementation:
+// Graph1:
+var startGraphOne: [String: [String: Int]] = [:]
+startGraphOne["Start"] = ["A": 6, "B": 2]
+startGraphOne["A"] = ["Final": 1]
+startGraphOne["B"] = ["A": 3, "Final": 5]
+startGraphOne["Final"] = [:]
+
+var costsGraphOne: [String: Int] = [:]
+costsGraphOne["A"] = 6
+costsGraphOne["B"] = 2
+costsGraphOne["Final"] = Int.max
+
+var parentsGraphOne: [String: String] = [:]
+parentsGraphOne["A"] = "Start"
+parentsGraphOne["B"] = "Start"
+parentsGraphOne["Final"] = ""
+
+// Graph2:
+var startGraphTwo: [String: [String: Int]] = [:]
+startGraphTwo["Start"] = ["A": 5, "B": 2]
+startGraphTwo["A"] = ["C": 4, "D": 2]
+startGraphTwo["B"] = ["A": 8, "D": 7]
+startGraphTwo["C"] = ["Final": 3, "D": 6]
+startGraphTwo["D"] = ["Final": 1]
+startGraphTwo["Final"] = [:]
+
+var costsGraphTwo: [String: Int] = [:]
+costsGraphTwo["A"] = 5
+costsGraphTwo["B"] = 2
+costsGraphTwo["C"] = Int.max
+costsGraphTwo["D"] = Int.max
+costsGraphTwo["Final"] = Int.max
+
+var parentsGraphTwo: [String: String] = [:]
+parentsGraphTwo["A"] = "Start"
+parentsGraphTwo["B"] = "Start"
+parentsGraphTwo["C"] = ""
+parentsGraphTwo["D"] = ""
+parentsGraphTwo["Final"] = ""
+
+// Graph3:
+var startGraphThree: [String: [String: Int]] = [:]
+startGraphThree["Start"] = ["A": 10]
+startGraphThree["A"] = ["B": 20]
+startGraphThree["B"] = ["C": 1, "Final": 30]
+startGraphThree["C"] = ["A": 1]
+startGraphThree["Final"] = [:]
+
+var costsGraphThree: [String: Int] = [:]
+costsGraphThree["A"] = 10
+costsGraphThree["B"] = Int.max
+costsGraphThree["C"] = Int.max
+costsGraphThree["Final"] = Int.max
+
+var parentsGraphThree: [String: String] = [:]
+parentsGraphThree["A"] = "Start"
+parentsGraphThree["B"] = ""
+parentsGraphThree["C"] = ""
+parentsGraphThree["Final"] = ""
+
+
+var startGraph: [String: [String: Int]] = [:]
+var costsGraph: [String: Int] = [:]
+var parentsGraph: [String: String] = [:]
+
+
+/// Set save only unic items.
+var processed: Set<String> = []
+
+// Method for finding lowest cost's node:
+private func lowestCostNode(_ costs: [String: Int]) -> String {
+    var lowestCost = Int.max
+    var lowestNode = ""
+    
+    costs.forEach {
+        guard $0.value < lowestCost, !processed.contains($0.key) else { return }
+        lowestCost = $0.value
+        lowestNode = $0.key
+    }
+    return lowestNode
+}
+
+// Method for finding lowest cost of any graphs:
+private func findLowestCost(in costs: [String: Int]) -> Int {
+    /// Current node.
+    var node = lowestCostNode(costs)
+    
+    while !node.isEmpty {
+        /// Node's cost.
+        let cost = costsGraph[node]!
+        /// Node's neighbors.
+        let neighbors = startGraph[node]!
+        
+        neighbors.forEach {
+            /// New cost.
+            let newCost = cost + $0.value
+            /// Check on max 'cost' in costsGraph.
+            guard costsGraph[$0.key]! > newCost else { return }
+            /// If checking is true - performing these actions.
+            costsGraph[$0.key]! = newCost
+            parentsGraph[$0.key]! = node
+        }
+        processed.insert(node)
+        node = lowestCostNode(costsGraph)
+    }
+    return costsGraph["Final"]!
+}
+
+//1
+startGraph = startGraphOne
+costsGraph = costsGraphOne
+parentsGraph = parentsGraphOne
+
+//2
+//startGraph = startGraphTwo
+//costsGraph = costsGraphTwo
+//parentsGraph = parentsGraphTwo
+
+//3
+//startGraph = startGraphThree
+//costsGraph = costsGraphThree
+//parentsGraph = parentsGraphThree
+
+findLowestCost(in: startGraph["Start"] ?? [:]) // Result => parents' graph: '["A": "B", "Final": "A", "B": "Start"]'
+                                                // Result => 'final' is 6
