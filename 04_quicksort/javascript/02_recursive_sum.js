2dbff96d23c9ba3dc3834b907d3ad568ed4808b6
Viacheslav <vyac.v.kuznetsov@rt.ru>
2022-09-26 13:46:16 +0300

    adding basic case to js example

--- 04_quicksort/javascript/02_recursive_sum.js
+++ 04_quicksort/javascript/02_recursive_sum.js
@@ -6,6 +6,7 @@
  * @returns {number} Sum of the numbers
  */
 function sumRecursive(array) {
+  if (!array.length) return 0;
   if (array.length == 1) return array[0];
   return array[0] + sumRecursive(array.slice(1));
 }
