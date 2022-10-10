73496836eb81ebeb957286053c30e4b2e71efda0
Paolo Grisoli <paolo_g84@icloud.com>
2022-10-11 00:25:55 +0200

    fix zig greedy algorithm

--- 08_greedy_algorithms/zig/set_covering.zig
+++ 08_greedy_algorithms/zig/set_covering.zig
@@ -4,49 +4,49 @@ const mem = std.mem;
 
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
-    var arena = heap.ArenaAllocator.init(&gpa.allocator);
+    var arena = heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
 
     var states_needed_array = [_][]const u8{ "mt", "wa", "or", "id", "nv", "ut", "ca", "az" };
-    var states_needed = std.BufSet.init(&arena.allocator);
+    var states_needed = std.BufSet.init(arena.allocator());
     for (states_needed_array) |sn| {
-        try states_needed.put(sn);
+        try states_needed.insert(sn);
     }
 
-    var stations = std.StringHashMap(*std.BufSet).init(&arena.allocator);
+    var stations = std.StringHashMap(*std.BufSet).init(arena.allocator());
 
-    var kone = std.BufSet.init(&arena.allocator);
-    try kone.put("id");
-    try kone.put("nv");
-    try kone.put("ut");
+    var kone = std.BufSet.init(arena.allocator());
+    try kone.insert("id");
+    try kone.insert("nv");
+    try kone.insert("ut");
     try stations.put("kone", &kone);
 
-    var ktwo = std.BufSet.init(&arena.allocator);
-    try ktwo.put("wa");
-    try ktwo.put("id");
-    try ktwo.put("mt");
+    var ktwo = std.BufSet.init(arena.allocator());
+    try ktwo.insert("wa");
+    try ktwo.insert("id");
+    try ktwo.insert("mt");
     try stations.put("ktwo", &ktwo);
 
-    var kthree = std.BufSet.init(&arena.allocator);
-    try kthree.put("or");
-    try kthree.put("nv");
-    try kthree.put("ca");
+    var kthree = std.BufSet.init(arena.allocator());
+    try kthree.insert("or");
+    try kthree.insert("nv");
+    try kthree.insert("ca");
     try stations.put("kthree", &kthree);
 
-    var kfour = std.BufSet.init(&arena.allocator);
-    try kfour.put("nv");
-    try kfour.put("ut");
+    var kfour = std.BufSet.init(arena.allocator());
+    try kfour.insert("nv");
+    try kfour.insert("ut");
     try stations.put("kfour", &kfour);
 
-    var kfive = std.BufSet.init(&arena.allocator);
-    try kfive.put("ca");
-    try kfive.put("az");
+    var kfive = std.BufSet.init(arena.allocator());
+    try kfive.insert("ca");
+    try kfive.insert("az");
     try stations.put("kfive", &kfive);
 
-    try setCovering(&gpa.allocator, &stations, &states_needed);
+    try setCovering(gpa.allocator(), &stations, &states_needed);
 }
 
-fn setCovering(allocator: *mem.Allocator, stations: *std.StringHashMap(*std.BufSet), states_needed: *std.BufSet) !void {
+fn setCovering(allocator: mem.Allocator, stations: *std.StringHashMap(*std.BufSet), states_needed: *std.BufSet) !void {
     var final_stations = std.BufSet.init(allocator);
     defer final_stations.deinit();
 
@@ -57,30 +57,30 @@ fn setCovering(allocator: *mem.Allocator, stations: *std.StringHashMap(*std.BufS
         var it = stations.iterator();
         while (it.next()) |element| {
             var covered = &std.ArrayList([]const u8).init(allocator);
-            try intersect(allocator, states_needed, element.value, covered);
+            try intersect(states_needed, element.value_ptr.*, covered);
             if (covered.items.len > states_covered.len) {
-                best_station = element.key;
+                best_station = element.key_ptr.*;
                 states_covered = covered.items;
             } else covered.deinit();
         }
 
         difference(states_needed, states_covered);
-        try final_stations.put(best_station);
+        try final_stations.insert(best_station);
     }
 
     var i = final_stations.iterator();
-    while (i.next()) |element| {
-        std.debug.print("{s}\n", .{element.key});
+    while (i.next()) |key| {
+        std.debug.print("{s}\n", .{key.*});
     }
 }
 
-fn intersect(allocator: *mem.Allocator, left: *std.BufSet, right: *std.BufSet, intersection: *std.ArrayList([]const u8)) !void {
+fn intersect(left: *std.BufSet, right: *std.BufSet, intersection: *std.ArrayList([]const u8)) !void {
     var l_it = left.iterator();
     var r_it = right.iterator();
-    while (l_it.next()) |l_elem| {
-        while (r_it.next()) |r_elem| {
-            if (std.mem.eql(u8, l_elem.key, r_elem.key)) {
-                try intersection.append(l_elem.key);
+    while (l_it.next()) |l_key| {
+        while (r_it.next()) |r_key| {
+            if (std.mem.eql(u8, l_key.*, r_key.*)) {
+                try intersection.append(l_key.*);
             }
         }
     }
@@ -89,10 +89,10 @@ fn intersect(allocator: *mem.Allocator, left: *std.BufSet, right: *std.BufSet, i
 fn difference(lessening: *std.BufSet, subtracting: [][]const u8) void {
     var less_it = lessening.iterator();
 
-    while (less_it.next()) |less_elem| {
-        for (subtracting) |sub_elem| {
-            if (std.mem.eql(u8, less_elem.key, sub_elem)) {
-                lessening.delete(less_elem.key);
+    while (less_it.next()) |less_key| {
+        for (subtracting) |sub_key| {
+            if (std.mem.eql(u8, less_key.*, sub_key)) {
+                lessening.remove(less_key.*);
             }
         }
     }
