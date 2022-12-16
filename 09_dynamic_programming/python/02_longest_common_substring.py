76b016ad8a147ab6c3d516e3728f204af73cd380
XiayongLee <71315026+Case3y@users.noreply.github.com>
2022-12-16 21:17:10 +0800

    Fix index location error

--- 09_dynamic_programming/python/02_longest_common_substring.py
+++ 09_dynamic_programming/python/02_longest_common_substring.py
@@ -1,13 +1,13 @@
 dp_table_blue = ["b", "l", "u", "e"]
 dp_table_clues = ["c", "l", "u", "e", "s"]
-dp_table = [[0 for i in range(len(dp_table_blue))] for i in range(len(dp_table_clues))] # (5,4)
-print(dp_table)
+dp_table = [[0 for i in range(len(dp_table_blue))] for i in range(len(dp_table_clues))]
+print(dp_table)  
 
-for i in range(0, len(dp_table_blue)):
-    for j in range(0, len(dp_table_clues)):
-        if dp_table_clues[j] == dp_table_blue[i]:
-            dp_table[j][i] = dp_table[i-1][i-1] + 1
+for i in range(0, len(dp_table_clues)):
+    for j in range(0, len(dp_table_blue)):
+        if dp_table_clues[i] == dp_table_blue[j]:
+            dp_table[i][j] = dp_table[i - 1][j - 1] + 1
         else:
-            dp_table[j][i] = 0
+            dp_table[i][j] = 0
 
 print(dp_table)
