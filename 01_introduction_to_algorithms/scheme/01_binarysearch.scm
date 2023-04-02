738eb7f947895f1f21141bcc2ef94c08b8fd3608
AslanAV <autlevaslan@yandex.ru>
2023-04-03 00:47:44 +0300

    add  01_binary_search.scm in 01_introduction_to_algorithms

---
+++ 01_introduction_to_algorithms/scheme/01_binarysearch.scm
@@ -0,0 +1,22 @@
+(display "01_binarysearch")
+
+(define (binary-search my-list item)
+  (iter my-list item 0 (- (length my-list) 1)))
+
+(define (iter my-list item low high)
+  (if (> low high) 'nill
+    (let* ((mid (floor (/ (+ low high) 2)))
+          (guess (list-ref my-list mid)))
+      (cond ((eqv? guess item) mid)
+              ((> guess item) (iter my-list item low (- mid 1)))
+              (else (iter my-list item (+ mid 1) high))))))
+
+
+(newline)
+(newline)
+(display (binary-search (list 1 3 5 7 9) 3))
+(newline)
+(newline)
+(display (binary-search (list 1 3 5 7 9) -1))
+(newline)
+(newline)
