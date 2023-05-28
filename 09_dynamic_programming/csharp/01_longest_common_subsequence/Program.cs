bce80edd94ac0354179685255488a8a122c2559b
MARCUS VOGADO <107502578+MarcusVogado@users.noreply.github.com>
2023-05-28 15:50:05 -0300

    Update Program.cs
    Hello, my name is Marcus Vogado.
    I'm a student and I bought your book, I noticed that in chapter 9 the example in the book is incomplete in the C# language. so I decided to put the studies into practice and updated the code. Thank you very much for the teachings.

--- 09_dynamic_programming/csharp/01_longest_common_subsequence/Program.cs
+++ 09_dynamic_programming/csharp/01_longest_common_subsequence/Program.cs
@@ -6,14 +6,66 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            var array = LongestCommom("hish", "fish");
+            //print array
+            PrintArrayLogestCommom(array);
+            
+            
+        }
+        public static int[,] LongestCommom(String wordA, String wordB)
+        {
+            int[,] cell = new int[wordA.Length, wordB.Length];
 
-            if (word_a[i] == word_b[1])
+            for (int i = 0; i < wordA.Length; i++)
             {
-                cell[i][j] = cell[i - 1][j - 1] + 1;
+                for (int j = 0; j < wordB.Length; j++)
+                {
+                    // The letters match
+                    if (wordA[i] == wordB[j])
+                    {
+                        if (i > 0 && j > 0)
+                        {
+                            cell[i, j] = cell[i - 1, j - 1] + 1;
+                        }
+                        else
+                        {
+                            cell[i, j] = 1;
+                        }
+                    }
+                    else
+                    {
+                        // The letters don't match.
+                        if (i == 0 && j > 0)
+                        {
+                            cell[i, j] = cell[i, j - 1];
+                        }
+                        else if (i > 0 && j == 0)
+                        {
+                            cell[i, j] = cell[i - 1, j];
+                        }
+                        else if (i > 0 && j > 0)
+                        {
+                            cell[i, j] = Math.Max(cell[i - 1, j], cell[i, j - 1]);
+                        }
+                        else
+                        {
+                            cell[i, j] = 0;
+                        }
+                    }
+                }
             }
-            else
+            return cell;
+        }
+
+        public static void PrintArrayLogest(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                cell[i][j] = Math.Max(cell[i - 1][j], cell[i][j - 1]);
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
             }
         }
     }
