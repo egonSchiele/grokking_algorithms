533287d30f3246600dde4802ea32d50801e39f5a
Oyekola Toheeb <toheeb.olawale.to23@gmail.com>
2023-07-20 09:42:40 +0100

    update list not to be modified during recursion

--- 03_recursion/java/04_sum/src/Sum.java
+++ 03_recursion/java/04_sum/src/Sum.java
@@ -1,20 +1,19 @@
 import java.util.*;
 
 public class Sum {
-    public static int sum(ArrayList<Integer> num_list) {
+    public static int sum(ArrayList<Integer> num_list, int index) {
 
-        if (num_list.size() == 0) {
+        if (num_list.size() == index) {
             return 0;
         } else {
-            int num = num_list.get(0);
-            num_list.remove(0);
-            return num + sum(num_list);
+            int num = num_list.get(index);
+            return num + sum(num_list, index);
         }
 
     }
 
     public static void main(String[] args) {
-        int total = sum(new ArrayList<Integer>(Arrays.asList(2, 4, 6)));
+        int total = sum(new ArrayList<Integer>(Arrays.asList(2, 4, 6)), 0);
         System.out.println(total);
     }
 }
