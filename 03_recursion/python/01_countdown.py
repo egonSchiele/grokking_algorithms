f482a8bf36b4b46b54b694d4e34ab8028aeb024b
Eder Andrés Ávila Niño <ederandresan@gmail.com>
2023-09-03 12:46:10 -0500

    Ch03 Countdown: Print numbers on the same line avoiding invalid integers
    The current function `countdown()` prints integers equal and lesser than zero, which is not desired as shown in the book:
    
    ```text
    > 3...2...1
    ```
    
    I propose printing the numbers on the same line separated with ellipsis (...), except for number 1, which should be finished with a new line. Additionally, the base case should stop if the current number `i` is lesser than one.
    
    -	modified:   01_countdown.py
    
    ```python
    def countdown(i):
      # base case
      if i < 1:
        # Finish decreasing and printing the numbers
        print()
        return
      # recursive case
      else:
        # Separate each number with ellipsis (...), except for the number 1.
        element_separator = "..." if i > 1 else ""
        # Print the numbers on the same line
        print(i, end=element_separator)
        return countdown(i - 1)
    ```

--- 03_recursion/python/01_countdown.py
+++ 03_recursion/python/01_countdown.py
@@ -1,10 +1,36 @@
 def countdown(i):
   # base case
-  if i <= 0:
-    return 0
+  if i < 1:
+    # Finish decreasing and printing the numbers
+    print()
+    return
   # recursive case
   else:
-    print(i)
-    return countdown(i-1)
+    # Separate each number with ellipsis (...), except for the number 1.
+    element_separator = "..." if i > 1 else ""
+    # Print the numbers on the same line
+    print(i, end=element_separator)
+    return countdown(i - 1)
 
-countdown(5)
+
+def main():
+  print("Countdown for 10:")
+  countdown(10)
+
+  print("Countdown for 5:")
+  countdown(5)
+
+  print("Countdown for 3:")
+  countdown(3)
+
+  print("Countdown for 1:")
+  countdown(1)
+
+  print("Countdown for 0:")
+  countdown(0)
+
+  print("Countdown for -3:")
+  countdown(-3)
+
+
+main()
