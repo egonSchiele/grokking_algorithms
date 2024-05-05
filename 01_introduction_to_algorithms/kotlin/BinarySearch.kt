28770db07c667794bff5f4a8ff285f372ea187d2
foroogh varmazyar <f.varmazyar992@gmail.com>
2024-05-05 18:00:18 +0330

    Chang 'if-else' condition to 'when'

--- 01_introduction_to_algorithms/kotlin/BinarySearch.kt
+++ 01_introduction_to_algorithms/kotlin/BinarySearch.kt
@@ -1,12 +1,14 @@
-fun List<Int>.binarySearch(item: Int) : Int? {
+fun List<Int>.binarySearch(item: Int): Int? {
     var min = 0
     var max = lastIndex
     while (min <= max) {
-        val mid = (min + max)/2
+        val mid = (min + max) / 2
         val guess = this[mid]
-        if (guess == item) return mid
-        if (guess > item) max = mid -1
-        else min = mid + 1
+        when {
+            guess == item -> return mid
+            guess > item -> max = mid - 1
+            else -> min = mid + 1
+        }
     }
     return null
 }
@@ -18,4 +20,3 @@ val sortedArray = (0..100 step 3).toList()
 fun main(args: Array<String>) {
     print(sortedArray.binarySearch(6))
 }
-
