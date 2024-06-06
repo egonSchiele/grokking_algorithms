3474601354f11f13ee6a0b9c0e8cffbc3b77b01f
Mark M <chtozamm@gmail.com>
2024-06-06 15:22:36 +0300

    Add comments

--- 06_breadth-first_search/Golang/01_breadth-first_search.go
+++ 06_breadth-first_search/Golang/01_breadth-first_search.go
@@ -22,20 +22,21 @@ func main() {
 func search(name string) bool {
 	var search_queue []string
 	search_queue = append(search_queue, graph[name]...)
+	// This is how you keep track of which people you've searched before.
 	searched := make(map[string]bool)
 	var person string
 	for len(search_queue) != 0 {
 		person = search_queue[0]
 		search_queue = search_queue[1:]
+		// Only search this person if you haven't already searched them.
 		if !searched[person] {
 			if person_is_seller(person) {
 				println(person + " is mango seller!")
 				return true
 			}
-
 			search_queue = append(search_queue, graph[person]...)
+			// Marks this person as searched.
 			searched[person] = true
-
 		}
 	}
 	return false
