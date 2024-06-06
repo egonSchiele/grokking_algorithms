9cac7c3568f14efe82a15bd7bb60bee8591775a1
Mark M <chtozamm@gmail.com>
2024-06-06 16:10:31 +0300

    Replace snake with camel case, update print method

--- 06_breadth-first_search/Golang/01_breadth-first_search.go
+++ 06_breadth-first_search/Golang/01_breadth-first_search.go
@@ -1,9 +1,13 @@
 package main
 
-func person_is_seller(name string) bool {
+import "fmt"
+
+// isSeller return true if person's name ends with "m"
+func isSeller(name string) bool {
 	return name[len(name)-1] == 'm'
 }
 
+// Initialize new graph
 var graph = make(map[string][]string)
 
 func main() {
@@ -19,22 +23,23 @@ func main() {
 	search("you")
 }
 
+// search returns true if a seller is found among friends or friends of friends of the given person.
 func search(name string) bool {
-	var search_queue []string
-	search_queue = append(search_queue, graph[name]...)
+	var searchQueue []string
+	searchQueue = append(searchQueue, graph[name]...)
 	// This is how you keep track of which people you've searched before.
 	searched := make(map[string]bool)
 	var person string
-	for len(search_queue) != 0 {
-		person = search_queue[0]
-		search_queue = search_queue[1:]
+	for len(searchQueue) != 0 {
+		person = searchQueue[0]
+		searchQueue = searchQueue[1:]
 		// Only search this person if you haven't already searched them.
 		if !searched[person] {
-			if person_is_seller(person) {
-				println(person + " is mango seller!")
+			if isSeller(person) {
+				fmt.Println(person + " is mango seller!")
 				return true
 			}
-			search_queue = append(search_queue, graph[person]...)
+			searchQueue = append(searchQueue, graph[person]...)
 			// Marks this person as searched.
 			searched[person] = true
 		}
