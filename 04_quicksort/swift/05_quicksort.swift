2de3121f9962825e6d8e51f058c672923fe27571
DuduBertolDev <dudubertoldev@gmail.com>
2026-03-12 12:04:43 -0300

    Update 05_quicksort.swift for Best and Worst case

--- 04_quicksort/swift/05_quicksort.swift
+++ 04_quicksort/swift/05_quicksort.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-//The following implementation of quick sort is little more classic than described in the book, but we have two use this one because of some “slice” feature limitation with array on Swift 3. Main concept is the same
-func quicksort <T : Comparable> (_ array : [T]) -> [T] {
+//Worst case performance: O(n^2) when the smallest or largest element is always chosen as the pivot. This can happen when the input array is already sorted or reverse sorted.
+func quicksort_worst <T : Comparable> (_ array : [T]) -> [T] {
     // base case, arrays with 0 or 1 element are already "sorted"
     guard array.count > 1 else { return array }
-    // recursive case
+    // recursive case - 0th element for worst case
     let pivot = array[0]
     // sub-array of all the elements less than the pivot
     let less = array.filter { $0 < pivot }
@@ -12,7 +12,24 @@ func quicksort <T : Comparable> (_ array : [T]) -> [T] {
     let equal = array.filter { $0 == pivot }
     // sub-array of all the elements greater than the pivot
     let greater = array.filter { $0 > pivot }
-    return quicksort(less) + equal + quicksort(greater)
+    return quicksort_worst(less) + equal + quicksort_worst(greater)
 }
 
-print(quicksort([1, 5, 10, 25, 16, 1])) // => [1, 1, 5, 10, 16, 25]
+print(quicksort_worst([1, 5, 10, 25, 16, 1])) // => [1, 1, 5, 10, 16, 25]
+
+//Best case performance: O(n log n) when the pivot divides the array into two equal
+func quicksort_best <T : Comparable> (_ array : [T]) -> [T] {
+    // base case, arrays with 0 or 1 element are already "sorted"
+    guard array.count > 1 else { return array }
+    // recursive case - random element for best case
+    let pivot = array[Int.random(in: 0 ..< array.count)]
+    // sub-array of all the elements less than the pivot
+    let less = array.filter { $0 < pivot }
+    // sub-array of all the elements equal to the pivot
+    let equal = array.filter { $0 == pivot }
+    // sub-array of all the elements greater than the pivot
+    let greater = array.filter { $0 > pivot }
+    return quicksort_best(less) + equal + quicksort_best(greater)
+}
+
+print(quicksort_best([1, 5, 10, 25, 16, 1])) // => [1, 1, 5, 10, 16, 25]
